c2b92206b0dd74e54ab93b5c7a3a0d7e016eb8b1
Tian0602 <tianning@kth.se>
2025-05-01 12:30:01 +0200

    Initial Commit

---
+++ TikTacToe/TikTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTacToeApp.swift
+//  TikTacToe
+//
+//  Created by 梁天宁 on 2025/5/1.
+//
+
+import SwiftUI
+
+@main
+struct TikTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTacToeTests/TikTacToeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TikTacToeTests.swift
+//  TikTacToeTests
+//
+//  Created by 梁天宁 on 2025/5/1.
+//
+
+import Testing
+
+struct TikTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
